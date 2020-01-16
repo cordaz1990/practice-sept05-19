1cfdc9db3a7b9303bb3ea94a28e6749a9cfb1dc2
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 19:44:40 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,14 @@
-print('hello')
-print('world')
-print('world')
+def __lt__(self, other):
+    #check the suits
+    if self.suit < other.suit: return True
+    if self.suit > other.suit: return False
+    
+    #suits are the sme ... check ranks
+    return self.rank <other.rank
+
+
+def __lt__(self, other):
+    t1 = self.suit, self.rank
+    t2 = other.suit, other.rank
+    return t1 < t2
+  
